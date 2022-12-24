ad876b14a83fb26a5a6bbeb4e00a93c0ae1f9df3
Gleb Koveshnikov <kovg16@gmail.com>
2022-12-24 21:05:51 +0400

    init more benchmark comparisons

--- benchmarks/tf_bert.py
+++ benchmarks/tf_bert.py
@@ -1,21 +1,54 @@
-import tensorflow as tf
-import tensorflow_text as tf_text
+import os
 import time
 
+import tensorflow
 
-def process(text, vocab_list):
+from tensorflow_text import BertTokenizer as TensorflowBertTokenizer
+from tokenizers import BertWordPieceTokenizer as HuggingFaceBertTokenizer
+from torchtext.transforms import BERTTokenizer as TorchBertTokenizer
+
+# TODO: check https://github.com/pytorch/text/blob/8eb056103cd1d518d53252dd63d3c75f284345ca/benchmark/benchmark_bert_tokenizer.py
+
+def run_tensorflow(text, vocab_file):
+    vocab_list = []
+    with open(vocab_file, 'r') as f:
+        for word in f:
+            vocab_list.append(word)
     lookup_table = tf.lookup.StaticVocabularyTable(
-        tf.lookup.KeyValueTensorInitializer(
+        tensorflow.lookup.KeyValueTensorInitializer(
             keys=vocab_list,
-            key_dtype=tf.string,
-            values=tf.range(
-                tf.size(vocab_list, out_type=tf.int64), dtype=tf.int64),
-            value_dtype=tf.int64
+            key_dtype=tensorflow.string,
+            values=tensorflow.range(
+                tensorflow.size(vocab_list, out_type=tensorflow.int64), dtype=tensorflow.int64),
+            value_dtype=tensorflow.int64
         ),
         num_oov_buckets=1
     )
-    bert_tokenizer = tf_text.BertTokenizer(lookup_table, token_out_type=tf.int64)
+    bert_tokenizer = TensorflowBertTokenizer(lookup_table, token_out_type=tensorflow.int64)
     return bert_tokenizer.tokenize(text).merge_dims(1, -1)
+
+
+def run_hugging_face(text, vocab_file):
+    tokenizer = HuggingFaceBertTokenizer(vocab_file)
+    print(tokenizer)
+    output = tokenizer.encode(text)
+    return output.ids
+
+
+def run_torch(text, vocab_file):
+    tokenizer = TorchBertTokenizer(vocab_file)
+    return tokenizer(text)
+
+
+def run_fastest_naive(text, vocab_file):
+    pass
+
+
+def run_benchmark(impls, text, vocab_file):
+    before = time.time_ns()
+    impl(text, vocab_list)
+    duration = time.time_ns() - before
+    print(duration)
 
 
 if __name__ == "__main__":
@@ -26,8 +59,10 @@ if __name__ == "__main__":
         "Patrick", "President", "Sp", "Sq", "bob", "box", "has", "highest",
         "is", "office", "the",
     ]
+    vocab_file = 'bert_vocab.txt'
+    with open(vocab_filename, 'w') as f:
+        for word in vocab_list:
+            f.write(f'{word}\n')
 
-    before = time.time_ns()
-    process(text, vocab_list)
-    duration = time.time_ns() - before
-    print(duration)
+    for impl in [run_hugging_face, run_tensorflow, run_torch, run_fastest_naive]:
+        run_benchmark(impl, text, vocab_file)
--- tests/stress.cpp
+++ tests/stress.cpp
@@ -34,6 +34,42 @@ std::vector<int> naiveTokenization(const std::string &s, const std::vector<std::
     return tokens;
 }
 
+std::vector<int> sublinearTokenization(const std::string &s, const std::vector<std::string> &vocab) {
+    std::unordered_map<std::string_view, int> word_to_id;
+    size_t max_len = 0;
+    for (int i = 0; i < static_cast<int64_t>(vocab.size()); i++) {
+        assert(!vocab[i].empty());
+        assert(word_to_id.count(vocab[i]) == 0);
+        word_to_id[vocab[i]] = i;
+        max_len = std::max(max_len, vocab[i].size());
+    }
+    max_len = std::min(max_len, s.size());
+
+    size_t start = 0;
+    size_t len = std::min(s.size(), max_len);
+
+    std::vector<int> tokens;
+    tokens.reserve(s.size() / max_len + 1);
+    while (len > 0) {
+        std::string_view test{s.data() + start, len};
+
+        auto it = word_to_id.find(test);
+        if (it != word_to_id.end()) {
+            tokens.push_back(it->second);
+            start += len;
+            len = std::min(max_len, s.size() - start);
+        } else {
+            --len;
+        }
+    }
+
+    if (start == s.size()) {
+        return tokens;
+    }
+
+    return {};
+}
+
 void compare(const std::string &s, const std::vector<std::string> &vocab, bool print_perf = false) {
     assert(verifyVocab(vocab));
     auto start = currentTs();
